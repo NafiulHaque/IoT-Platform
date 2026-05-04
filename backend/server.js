8e80b79fa9967e400ee635e4251e5169b34454b3
Nafiul Haque <nafiulhaque22@gmail.com>
2026-05-04 23:09:00 +0600

    backend server fix

--- backend/server.js
+++ backend/server.js
@@ -74,7 +74,7 @@ io.on('connection', (socket) => {
 connectDB().then(() => {
   connectMQTT(io);
   startOfflineDetector(io);
-  server.listen(process.env.PORT, () =>
-    console.log(`Server running on port ${process.env.PORT}`)
+  server.listen(process.env.PORT || 5000, () =>
+    console.log(`Server running on port ${process.env.PORT || 5000}`)
   );
 });
