effa81f31afa6c4800ca5fe336ee3ba7370754bf
Nafiul Haque <nafiulhaque22@gmail.com>
2026-05-04 21:21:05 +0600

    vite.config fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     outDir: 'dist',
     sourcemap: false,
     chunkSizeWarningLimit: 1000,
-    rolldownOptions:{
+    rollupOptions:{
       output:{
         manualChunks:{
            vendor:  ['react', 'react-dom', 'react-router-dom'],
